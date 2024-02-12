3a93935010457c7e904bc2666097c433abd6cf03
ahm-amdad <ah.milon.cse@gmail.com>
2024-02-12 23:41:22 +0600

    add triange area

--- script/triangle.js
+++ script/triangle.js
@@ -1,1 +1,14 @@
-console.log('connected js file')
+function calculateTriangleArea(){
+    const triangleAreaBase = document.getElementById('triangle-base');
+    const triangleBaseText = triangleAreaBase.value ;
+    const base = parseFloat(triangleBaseText);
+    
+    const triangleAreaHeight  = document.getElementById('triangle-height');
+    const triangleHeightText = triangleAreaHeight.value ;
+    const height = parseFloat(triangleHeightText)
+    
+    // triangle area calculate 
+    const area = 0.5 * base * height;
+    console.log("Area of the Triangle", area);
+    
+}
